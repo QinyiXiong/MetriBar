0c60456f4254f63cd910425f89d087c276e4cb53
覃义雄 <972354178@qq.com>
2026-09-25 12:50:20 +0800

    Initial Commit

---
+++ MetriBar/MetriBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetriBarApp.swift
+//  MetriBar
+//
+//  Created by 覃义雄 on 2026/9/25.
+//
+
+import SwiftUI
+
+@main
+struct MetriBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
